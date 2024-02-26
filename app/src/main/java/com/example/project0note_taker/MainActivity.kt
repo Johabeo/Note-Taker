9079d8ebf50d0aa83f76e6aef2cf9ae1ba58c876
John <Johhab10@gmail.com>
2024-02-26 03:05:09 -0800

    tutoring session 3

--- app/src/main/java/com/example/project0note_taker/MainActivity.kt
+++ app/src/main/java/com/example/project0note_taker/MainActivity.kt
@@ -10,10 +10,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.project0note_taker.ui.theme.Project0NoteTakerTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,9 +56,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val coroutine = rememberCoroutineScope()
+    var myText by remember{
+        mutableStateOf("Hello")
+    }
+    // unconfined - whatever thread is available
+    LaunchedEffect(key1 = Unit ){
+        coroutine.launch(Dispatchers.Unconfined) {
+            myText = "Goodbye"
+        }
+    }
+
+    //context vs scope
     Surface(color = Color.Blue) {
         Text(
-            text = "Hello $name!",
+            text = "Hello $myText!",
             modifier = modifier
         )
     }
